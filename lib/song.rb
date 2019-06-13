7c830182f8ac4173d13182b359033a4b9176e0d0
Jasmine Chen <jasmine1226@gmail.com>
2019-06-13 01:21:12 +0000

    Automatically backed up by Learn

--- lib/song.rb
+++ lib/song.rb
@@ -2,7 +2,8 @@ require 'pry'
 require_relative '../lib/concerns/memorable.rb'
 
 class Song
-  extend Memorable
+  extend Memorable, Findable
+  include Paramable
 
   attr_accessor :name
   attr_reader :artist
