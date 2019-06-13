8fca61f3fb3448afaaed069c36373e3c7b8d204e
Jasmine Chen <jasmine1226@gmail.com>
2019-06-13 01:28:39 +0000

    Automatically backed up by Learn

--- lib/concerns/memorable.rb
+++ lib/concerns/memorable.rb
@@ -1,16 +1,16 @@
 module Memorable
   module ClassModules
-    def initialize
-      self.class.all << self
-    end
-
     def reset_all
       self.all.clear
     end
-  end
-  module InstanceModules
     def count
       self.all.count
+    end
+  end
+  
+  module InstanceModules
+    def initialize
+      self.class.all << self
     end
   end
 end
